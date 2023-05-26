f2abc3ae761d2c4ae9d2de82f4a353b648aac235
RedRe4per <s3582474@gmail.com>
2023-05-26 19:03:36 +1000

    Initial commit

---
+++ hello-world/app.mjs
@@ -0,0 +1,31 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+export const lambdaHandler = async (event, context) => {
+    try {
+        return {
+            'statusCode': 200,
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PATCH,DELETE,PUT"
+            },
+            'body': JSON.stringify({
+                message: 'hello world 1111111111111112',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
